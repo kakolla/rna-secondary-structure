3004e258e428a397531bc7f14460f54066c55e8b
Abi Kakolla <abhishekkakolla1@gmail.com>
2025-05-10 00:57:16 -0700

    recurrence relation

--- fold.cpp
+++ fold.cpp
@@ -11,12 +11,20 @@ enum Nucleotides {
 
 // constraints:
 // no sharp turns: (b_i, b_j), i < j -4
-// no crossing over: (b_i, b_j), (b_k, b_l), i < j < k < l
+// no crossing over: (b_i, b_j), (b_k, b_l), i < j < k < l, or k < i < j < l 
 
+/*
+OPT(i, j) is max # of pairs of b_i, .. b_j
+OPT(i, j) = max {
+    OPT(i, j-1),
+    max_t {1 + OPT(i,t-1) + OPT(t+1, j-1)} , i <= t < j-4 (hairpin structure)
+}
+    
+*/
 // return max number of pairings
 int fold(vector<int> sequence) {
     
 
-
+    
     return -1;
 }
