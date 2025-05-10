6a1c979c67fa382068252daae66181b9f664c813
Abi Kakolla <abhishekkakolla1@gmail.com>
2025-05-09 23:42:11 -0700

    init

---
+++ fold.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+int main() {
+    // OPT(i, j) = max{OPT(j+1, j), max_t(1+OPT(i+1, j) + OPT(i+1, t-1))}
+    
+
+
+    return 0;
+}
