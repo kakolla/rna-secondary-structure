c3fb44159d52e9ad8e73fac33f42031a9c010de8
Abi Kakolla <abhishekkakolla1@gmail.com>
2025-05-10 00:18:49 -0700

    init fold

--- fold.cpp
+++ fold.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <vector>
 
-
 using namespace std;
+enum Nucleotides {
+    U,
+    A,
+    C,
+    G
+};
 
-int main() {
-    // OPT(i, j) = max{OPT(j+1, j), max_t(1+OPT(i+1, j) + OPT(i+1, t-1))}
+// constraints:
+// no sharp turns: (b_i, b_j), i < j -4
+// no crossing over: (b_i, b_j), (b_k, b_l), i < j < k < l
+
+// return max number of pairings
+int fold(vector<int> sequence) {
     
 
 
-    return 0;
+    return -1;
 }
